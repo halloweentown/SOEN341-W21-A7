742c01b1021f0c34cbbf6704b32960be038ce113
MahHairy <MahHairy@hotmail.com>
2021-02-16 16:39:13 -0500

    Revert "Limit the captions' number of words"
    This reverts commit b4d824899a81070b9fe74677b4ab40cbb3683076.

--- idk/resources/views/home.blade.php
+++ idk/resources/views/home.blade.php
@@ -11,30 +11,9 @@
                 <div class="card-body">
                     <form action="{{route('home')}}" method="POST" enctype="multipart/form-data">
                         {{ csrf_field() }} 
-                        <textarea name="body" id="caption" rows="3" cols="25" class="form-control" placeholder="Enter your caption here,upload your picture, then press submit!"></textarea>
+                        <textarea name="body" rows="3" cols="25" class="form-control" placeholder="Enter your caption here,upload your picture, then press submit!"></textarea>
                         <input type="file" name="image"/>
                         <input type="submit" name="upload" value="Upload"/>
-                        
-                        <script type="text/javascript">
-                            //get the text area element
-                            //keypress event for when user presses a key
-                            //add a function that keeps track of the input after a key is pressed
-                            document.getElementById("caption").addEventListener("keypress", function(event){
-
-                                //Split the words into different elements of an array when encountering at least one space character
-                                var words = this.value.split(/\s+/);
-                                //Get the number of words in that array
-                                var numWords = words.length;
-                                //Set the maximum number of words to write
-                                var maxWords = 250;
-
-                                //When maximum of words is exceeded
-                                if(numWords > maxWords){
-                                    //prevent further input
-                                    event.preventDefault();
-                                }
-                            });
-                        </script>
                     </form>
                 </div>
     
