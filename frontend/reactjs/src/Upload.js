c996dbd49ee2318454e1dda2b5953fabb1fdfca3
CSman22 <jackey211@hotmail.com>
2021-02-21 15:19:50 -0500

    Revert "Revert "Merge branch 'Seperate-login-page' into frontend_updated""
    This reverts commit eee8f8defd29c730230854f415602292b3d5d078.

---
+++ frontend/reactjs/src/Upload.js
@@ -0,0 +1,70 @@
+import React, { useState } from 'react'
+import Modal from './Modal'
+import './style/Upload.css';
+import Button from '@material-ui/core/Button';
+
+export default function Upload({username}) {
+    const [caption, setCaption] = useState('');
+    const [progress, setProgress] = useState(0);
+    const [image, setImage] = useState(null);
+
+    const handleChange = (e) =>{
+        if(e.target.file[0]){
+            setImage(e.target.files[0]);
+        }
+    }
+
+    const handleUpload = () => {
+        /*#TODO: take the image that you uploaded and store in database*/
+        //#TODO: bottom code to be uncommented after adding the code to store the image
+        //const uploadTask = [How we would store the data into a database]
+        /* uploadTask.on(
+            "state_changed",
+            (snapshot) => {
+            const progress = Math.round(
+                (snapshot.bytesTransferred / snapshot.totalBytes ) * 100
+            );
+            setProgress (progress);
+            },
+            (error) => {
+                console.log(error);
+                alert(error.message);
+            },
+            () =>{
+                storage
+                .ref("images")
+                .child(image.name)
+                .getDownloadURL()
+                .then(url =>{
+                    db.collection("posts").add({
+                        timestamp: //TODO: grab server timestamp,
+                        caption: caption,
+                        imageUrl: url,
+                        username: username
+                    })
+                })
+            }
+        ) */
+    }
+  const [isOpen, setIsOpen] = useState(false)
+  return (
+    <>
+      <div className ="upload__button" onClick={() => console.log('clicked')}>
+        <button onClick={() => setIsOpen(true)}>Upload</button>
+
+        <Modal open={isOpen} onClose={() => setIsOpen(false)}>
+            <div className = "upload">
+                <input type="text" placeholder='Enter a caption...' onChange={event => setCaption(event.target.value)} value={caption}/>
+                <div>
+                <input type="file" onChange={handleChange}/>
+                <Button onClick ={handleUpload}>
+                    Upload
+                </Button>
+            </div>
+            </div>
+        </Modal>
+      </div>
+
+    </>
+  )
+}
