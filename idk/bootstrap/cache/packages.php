5fede455af82154c1040c24a8a0b2dc6f9b85db1
MahHairy <MahHairy@hotmail.com>
2021-02-27 06:17:46 -0500

    Updating composer.json file
    Getting the latest version of dependencies

--- idk/bootstrap/cache/packages.php
+++ idk/bootstrap/cache/packages.php
@@ -24,6 +24,17 @@
       0 => 'Fruitcake\\Cors\\CorsServiceProvider',
     ),
   ),
+  'intervention/image' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Intervention\\Image\\ImageServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Image' => 'Intervention\\Image\\Facades\\Image',
+    ),
+  ),
   'laravel/sail' => 
   array (
     'providers' => 
