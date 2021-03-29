3e51c9f5462252d41137bf9259569342eb5480ae
giroux2000 <67913484+giroux2000@users.noreply.github.com>
2021-03-29 15:59:08 -0400

    Comments
    Wrote comments for all of the models

--- idk/app/Models/Profile.php
+++ idk/app/Models/Profile.php
@@ -5,10 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+//A model for the user profile.
 class Profile extends Model
 {
     use HasFactory;
 
+    //Makes it so that every profile belongs to a specific user.
     function user()
     {
         return $this->belongsTo(User::class);
--- idk/app/Models/User.php
+++ idk/app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
+//Model for the users.
 class User extends Authenticatable
 {
     use HasFactory, Notifiable;
@@ -16,6 +17,8 @@ class User extends Authenticatable
      *
      * @var array
      */
+
+     //Every uesr has a name, email, password, and avatar that is assigned to them during registration.
     protected $fillable = [
         'name',
         'email',
@@ -28,6 +31,8 @@ class User extends Authenticatable
      *
      * @var array
      */
+
+     //The passwords and remember tokens are hashed in the database.
     protected $hidden = [
         'password',
         'remember_token',
@@ -38,10 +43,12 @@ class User extends Authenticatable
      *
      * @var array
      */
+    //This part of the code is not used in our implementation, was provided by Laravel.
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
 
+    //Makes it so that a user can have many posts attributed to them.
     function posts()
     {
         return $this->hasMany(Post::class)->orderBy('created_at','DESC');
@@ -49,15 +56,18 @@ class User extends Authenticatable
 
     }
 
+    //Makes it so that a user can have many comments attributed to them.
     function comments()
     {
         return $this->hasMany(Comment::class);
     }
 
+    //A user has many people that they are following.
     function following() {
         return $this->belongsToMany(User::class, 'followings', 'userFollowing', 'userFollowed');
     }
 
+    //A user has many followers.
     function followers() {
         return $this->belongsToMany(User::class, 'followings', 'userFollowed', 'userFollowing');
     }
@@ -70,16 +80,19 @@ class User extends Authenticatable
         return $this->following()->detach($post->userID);
     }
 */
+    //Affects the numbers of another user's followers based on if this user follows them or not.
     function isFollowing(Post $post)
     {
         return $this->following()->where('userFollowed', $post->userID)->count();
     }
 
+    //Affects the user's follower numbers.
     function isFollowingUser(User $user)
     {
         return $this->following()->where('userFollowed', $user->id)->count();
     }
 
+    //Makes it so that a user can only have one profile.
     function profile()
     {
         return $this->hasOne(Profile::class);
